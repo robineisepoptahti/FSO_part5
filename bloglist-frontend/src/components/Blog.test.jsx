76a057df58cee9de52047d6130874275a481f30f
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2025-02-22 11:56:27 +0200

    Added tests 5.14

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -1,25 +1,45 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
-import { vi } from 'vitest'
+import { describe, vi } from 'vitest'
+import userEvent from '@testing-library/user-event'
+import user from '../../../bloglist_backend/models/user'
 
 
-
-test('renders Blog title', async () => {
+describe('Blog component tests', () => {
+  const mockUpdateLikes = vi.fn()
+  const mockRemoveBlog = vi.fn()
   const mockBlog = {
     author: 'janne',
     title: 'Jannen kirja',
-    url: 'www.test.fi'
+    url: 'www.test.fi',
+    likes: 5,
+    user: {
+      name: 'jannesson',
+      username: 'janskuli'
+    }
   }
-  const mockUpdateLikes = vi.fn()
-  const mockRemoveBlog = vi.fn()
-  render(<Blog blog={mockBlog} updateLikes={mockUpdateLikes} removeBlog={mockRemoveBlog} />)
-  const author = screen.getByText('janne', { exact: false })
-  const title = screen.getByText('Jannen kirja', { exact: false })
-  const likes = screen.queryByText('likes', { exact: false })
-  const url = screen.queryByText('www.test.fi', { exact: false })
-  screen.debug()
-  expect(author).toBeDefined()
-  expect(title).toBeDefined()
-  expect(likes).toBeNull()
-  expect(url).toBeNull()
+  test('renders Blog title', async () => {
+    render(<Blog blog={mockBlog} updateLikes={mockUpdateLikes} removeBlog={mockRemoveBlog} />)
+    const author = screen.getByText('janne', { exact: false })
+    const title = screen.getByText('Jannen kirja', { exact: false })
+    const likes = screen.queryByText('likes', { exact: false })
+    const url = screen.queryByText('www.test.fi', { exact: false })
+    screen.debug()
+    expect(author).toBeDefined()
+    expect(title).toBeDefined()
+    expect(likes).toBeNull()
+    expect(url).toBeNull()
+  })
+
+  test('see if likes and url are rendered after opening expanded view', async () => {
+    render(<Blog blog={mockBlog} updateLikes={mockUpdateLikes} removeBlog={mockRemoveBlog} />)
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+    const likes = screen.getByText('likes', { exact: false })
+    const url = screen.getByText('www.test.fi', { exact: false })
+    screen.debug()
+    expect(likes).toBeDefined()
+    expect(url).toBeDefined()
+  })
 })
